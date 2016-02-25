900fad078e9c07891684bebf2e170a5b23f39306
shelling <navyblueshellingford@gmail.com>
2016-02-25 10:09:00 -0800

    add skeleton

---
+++ app/javascripts/components/Schedules/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { getLocale } from "javascripts/locale";
+import styles from "./styles.css";
+
+class Schedules extends Component {
+  render() {
+    return (
+      <div className={styles.root}>
+      </div>
+    )
+  }
+}
+
+export default Schedules;
